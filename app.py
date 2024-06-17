5c5fecc4b0119a07bf00d24cd6486331f03ddf7e
LalithAravindan21 <122786418+LalithAravindan21@users.noreply.github.com>
2024-06-17 10:17:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 def get_gemini_response(jd, resume_text):
     model = genai.GenerativeModel('gemini-pro')
-    input_prompt = f"""
+   input_prompt = f"""
     {jd}
     
     Resume:
@@ -53,7 +53,7 @@ def get_gemini_response(jd, resume_text):
                 "Question": "Technical question 2",
                 "Keywords": ["Keyword3", "Keyword4"]
             }}
-            // at least 5 questions as required
+            // more questions as required
         ],
         "Potential_Interview_Questions": [
             {{
@@ -64,7 +64,7 @@ def get_gemini_response(jd, resume_text):
                 "Question": "Interview question 2",
                 "Keywords": ["Keyword3", "Keyword4"]
             }}
-            // at least 5 questions as required
+            // more questions as required
         ],
         "Technical_Projects_Required": [
             "Project mentioned in the resume",
@@ -100,20 +100,19 @@ def get_gemini_response(jd, resume_text):
                 "Practice question 1 for Project_Name_1",
                 "Practice question 2 for Project_Name_1",
                 "Practice question 3 for Project_Name_1",
-                "Practice question 4 for Project_Name_1",
-                "Practice question 5 for Project_Name_1"
+                "Practice question 4 for Project_Name_1"
             ],
             "Project_Name_2": [
                 "Practice question 1 for Project_Name_2",
                 "Practice question 2 for Project_Name_2",
                 "Practice question 3 for Project_Name_2",
-                "Practice question 4 for Project_Name_2",
-                "Practice question 5 for Project_Name_2"
+                "Practice question 4 for Project_Name_2"
             ]
             // more projects and questions as required
         }}
     }}
     """
+
 
     response = model.generate_content(input_prompt)
     return response.text
